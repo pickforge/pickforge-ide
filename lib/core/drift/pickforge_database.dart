c7aaa53b2e20d8cd176b0685f743ba66fe466b54
Elberte Plínio <elberte.dev@gmail.com>
2026-04-25 18:36:40 -0300

    feat(drift): add Projects table and dao

--- lib/core/drift/pickforge_database.dart
+++ lib/core/drift/pickforge_database.dart
@@ -4,15 +4,17 @@ import 'package:injectable/injectable.dart';
 import 'package:pickforge/core/drift/dao/agent_run_log_dao.dart';
 import 'package:pickforge/core/drift/dao/pick_history_dao.dart';
 import 'package:pickforge/core/drift/dao/project_settings_dao.dart';
+import 'package:pickforge/core/drift/dao/projects_dao.dart';
 import 'package:pickforge/core/drift/tables/agent_run_log.dart';
 import 'package:pickforge/core/drift/tables/pick_history.dart';
 import 'package:pickforge/core/drift/tables/project_settings.dart';
+import 'package:pickforge/core/drift/tables/projects.dart';
 
 part 'pickforge_database.g.dart';
 
 @DriftDatabase(
-  tables: [ProjectSettings, PickHistory, AgentRunLog],
-  daos: [ProjectSettingsDao, PickHistoryDao, AgentRunLogDao],
+  tables: [ProjectSettings, PickHistory, AgentRunLog, Projects],
+  daos: [ProjectSettingsDao, PickHistoryDao, AgentRunLogDao, ProjectsDao],
 )
 @lazySingleton
 class PickforgeDatabase extends _$PickforgeDatabase {
